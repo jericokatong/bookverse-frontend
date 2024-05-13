b272384cd705c83c077e9876f3c4fc6cb7d1b9e6
jericokatong <jericokatong@gmail.com>
2024-05-13 09:16:10 +0800

    update login page

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -71,7 +71,7 @@ const Login = () => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            Sign in to your account update 1
           </h2>
         </div>
         <form className="mt-8 space-y-6 flex flex-col">
